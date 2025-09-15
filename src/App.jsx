a81f12f7779c477bee11e5adddc71be9fc484a76
ayyushhere <ayushkumar0211a@gmail.com>
2025-09-16 00:09:16 +0530

    Made changes in css of App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { setLocalStorage } from './utils/localStorage'
 import { useEffect } from 'react'
 import { getLocalStorage } from './utils/localStorage'
+import { useState } from 'react'
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage();
-    getLocalStorage();
-  },);
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (email, password) =>{
+    if(email == 'admin@me.com' && password == '123'){
+      setUser('admin');
+      console.log(user);
+    } else if(email == 'user@me.com' && password == '123'){
+      setUser('employee');
+    } else{
+      alert("Invalid credentials");
+    }
+  }
   
 
   return (
     <>
-      <Login></Login>
-      {/* <EmployeeDashboard></EmployeeDashboard> */}
-      {/* <AdminDashboard></AdminDashboard> */}
+      {!user ? <Login handleLogin={handleLogin}></Login> : ''}
+      {user == 'admin' ? <AdminDashboard></AdminDashboard> : <EmployeeDashboard></EmployeeDashboard>}
     </>
   )
 }
